a51fe45a93cb29ceefee8e290f578c01776386fc
Reza <reza.asad@math.ubc.ca>
2015-03-22 20:28:50 -0700

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-chmod a+x ./src/word_count.py
-chmod a+x ./src/running_median.py
-
-python ./src/word_count.py >./wc_output/wc_result.txt
-python ./src/running_median.py >./wc_output/med_result.txt
